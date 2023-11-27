e45872d5c89f2af71f506c21a08a3e25f85034e2
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-11-27 16:48:38 -0300

    Update

--- WinFormsAppPractice/Win Forms App Practice 01/FrmPractice01.Designer.cs
+++ WinFormsAppPractice/Win Forms App Practice 01/FrmPractice01.Designer.cs
@@ -28,19 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnIngresar = new Button();
+            txtNombre = new TextBox();
+            lblNombre = new Label();
+            numEdad = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numEdad).BeginInit();
             SuspendLayout();
             // 
-            // Form1
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(54, 204);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(100, 26);
+            btnIngresar.TabIndex = 0;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = true;
+            btnIngresar.Click += btnIngresar_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(54, 106);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 20);
+            txtNombre.TabIndex = 1;
+            txtNombre.TextChanged += textBox1_TextChanged;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNombre.Location = new Point(54, 68);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(93, 25);
+            lblNombre.TabIndex = 2;
+            lblNombre.Text = "Nombre";
+            lblNombre.Click += label1_Click;
+            // 
+            // numEdad
+            // 
+            numEdad.Location = new Point(54, 152);
+            numEdad.Name = "numEdad";
+            numEdad.Size = new Size(120, 20);
+            numEdad.TabIndex = 3;
+            // 
+            // FrmPractice01
             // 
             AutoScaleDimensions = new SizeF(6F, 13F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(686, 390);
+            ClientSize = new Size(219, 262);
+            Controls.Add(numEdad);
+            Controls.Add(lblNombre);
+            Controls.Add(txtNombre);
+            Controls.Add(btnIngresar);
             Font = new Font("Microsoft Sans Serif", 8.25F);
-            Name = "Form1";
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "FrmPractice01";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Project01";
+            ((System.ComponentModel.ISupportInitialize)numEdad).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnIngresar;
+        private TextBox txtNombre;
+        private Label lblNombre;
+        private NumericUpDown numEdad;
     }
 }
--- WinFormsAppPractice/Win Forms App Practice 01/FrmPractice01.cs
+++ WinFormsAppPractice/Win Forms App Practice 01/FrmPractice01.cs
@@ -6,5 +6,21 @@ namespace Win_Forms_App_Practice_01
         {
             InitializeComponent();
         }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtNombre.Text);
+            MessageBox.Show(numEdad.Value.ToString());
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
